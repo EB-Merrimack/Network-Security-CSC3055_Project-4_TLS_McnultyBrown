1a798bf5f36c6f2cba2581d2b8aa1ef70dd9ad5a
mcnultye <157040192+mcnultye@users.noreply.github.com>
2025-04-09 17:31:20 -0400

    creating public key request class

--- src/common/protocol/messages/PubKeyRequest.java
+++ src/common/protocol/messages/PubKeyRequest.java
@@ -1,15 +1,54 @@
 package common.protocol.messages;
 
-public class PubKeyRequest {
-    public String type;
-    public String user;
+import common.protocol.Message;
+import merrimackutil.json.types.JSONType;
+import merrimackutil.json.types.JSONObject;
+import java.io.InvalidObjectException;
 
-    public PubKeyRequest() {
-        this.type = "PubKeyRequest";
-    }
+public class PubKeyRequest implements Message {
+    private String user;
+
+    public PubKeyRequest() {}
 
     public PubKeyRequest(String user) {
-        this.type = "PubKeyRequest";
         this.user = user;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    @Override
+    public String getType() {
+        return "PubKeyRequest";
+    }
+
+    @Override
+    public JSONType toJSONType() {
+        JSONObject obj = new JSONObject();
+        obj.put("type", "PubKeyRequest");
+        obj.put("user", user);
+        return obj;
+    }
+
+    @Override
+    public void deserialize(JSONType obj) throws InvalidObjectException {
+        if (!(obj instanceof JSONObject)) {
+            throw new InvalidObjectException("Expected JSONObject.");
+        }
+
+        JSONObject json = (JSONObject) obj;
+        this.user = json.getString("user");
+    }
+
+    @Override
+    public Message decode(JSONObject obj) throws InvalidObjectException {
+        String user = obj.getString("user");
+        return new PubKeyRequest(user);
+    }
+
+    @Override
+    public String toString() {
+        return "[PubKeyRequest] user=" + user;
     }
 }
